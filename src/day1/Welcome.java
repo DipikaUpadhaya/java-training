a69c2786e6fd5741d845adedf026837252b1746d
Dipika Upadhaya <ramesh.sigdel@infoworks-tn.com>
2021-12-24 09:15:44 -0600

    Initial Commit

---
+++ src/day1/Welcome.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class Welcome {
+    public static void main(String[] args){
+        String txt= "Welcome to the world of java programming";
+        System.out.println(txt);
+        for(int i=0; i<txt.length(); i++){
+            System.out.print("*");
+        }
+
+    }
+}
