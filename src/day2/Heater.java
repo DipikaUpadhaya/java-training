b67cfd861013a5ed118d7c6c86a1c82418d566a0
Dipika Upadhaya <ramesh.sigdel@infoworks-tn.com>
2021-12-28 15:04:29 -0600

    classes concept

---
+++ src/day2/Heater.java
@@ -0,0 +1,33 @@
+package day2;
+
+public class Heater {
+    // STATES
+
+    // brand
+    // color
+    // model
+    // size
+    // powerConsumption
+    // electricOrGas
+    // portable
+    // price
+    // numberOfButtons
+
+    String brand;
+    String color;
+    String model;
+    String size;
+    float powerConsumption;
+    String electricOrGas;
+    boolean isPortable;
+    float price;
+    short numberOfButtons;
+
+
+
+    // behaviour
+
+    // rotate
+    // on
+    // off
+}
